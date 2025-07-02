4843841057a6276468277f96738ff14dc642ac56
Bahaa Mekkawy <bahaa.elmekkawy@gmail.com>
2025-07-03 00:04:15 +0300

    Time: 0 ms (100%), Space: 22 MB (74.04%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -9,6 +9,17 @@
     // Left-moving asteroids (< 0) trigger collision checks
     // Size comparisons determine destruction
     // Final stack is reversed for correct order
+
+//Ex. [10,5,-3,-50]
+//First stack is empty => push 10 [10]
+//5 is not negative => push 5 [10,5]
+//-3 is negative so check its mass with the top of stack => don't push -3 [10,5]
+//-50 is negative so check its mass with the top of stack => pop 5 and check again until stack is empty [10]
+//-50 is negative so check its mass with the top of stack => pop 10 end of while loop flag changed and push -50 [-50]
+//return [-50]
+
+
+
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
