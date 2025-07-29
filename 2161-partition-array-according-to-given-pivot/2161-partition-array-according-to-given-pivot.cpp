260da6f3fa2490a420b22033109c355e23cce690
Bahaa Mekkawy <bahaa.elmekkawy@gmail.com>
2025-07-30 00:01:44 +0300

    Time: 4 ms (78.04%), Space: 140.9 MB (17.98%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -20,3 +20,31 @@ public:
         return res;
     }
 };
+
+//Another Approach (Two Pointers)
+    // vector<int> pivotArray(vector<int>& nums, int pivot) {
+    //     int n = nums.size();
+    //     vector<int> res(n);
+    //     int left = 0, right = n - 1;
+
+    //     // First pass: place all elements > pivot at the end (right pointer)
+    //     for (int i = n - 1; i >= 0; --i) {
+    //         if (nums[i] > pivot) {
+    //             res[right--] = nums[i];
+    //         }
+    //     }
+
+    //     // Second pass: place all elements < pivot at the beginning (left pointer)
+    //     for (int i = 0; i < n; ++i) {
+    //         if (nums[i] < pivot) {
+    //             res[left++] = nums[i];
+    //         }
+    //     }
+
+    //     // Fill pivot values in the middle
+    //     while (left <= right) {
+    //         res[left++] = pivot;
+    //     }
+
+    //     return res;
+    // }
