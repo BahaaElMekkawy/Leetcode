c942bd9747a7f11ebb50dd96b05f4d9409033fa4
Bahaa Mekkawy <bahaa.elmekkawy@gmail.com>
2025-09-28 23:48:56 +0300

    Time: 0 ms (100%), Space: 20.2 MB (52.11%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -1,22 +1,34 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        if (head == nullptr)
-            return head;
-        if (head->val == val) {
-            return removeElements(head->next, val);
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+
+        ListNode* current = dummy;
+        while (current->next != nullptr) {
+            if (current->next->val == val) {
+                ListNode* temp = current->next;
+                current->next = current->next->next;
+                delete temp; 
+            } else {
+                current = current->next;
+            }
         }
-        head->next = removeElements(head->next, val);
+
+        head = dummy->next;
+        delete dummy; 
         return head;
     }
 };
+// class Solution {
+// public:
+//     ListNode* removeElements(ListNode* head, int val) {
+//         if (head == nullptr)
+//             return head;
+//         if (head->val == val) {
+//             return removeElements(head->next, val);
+//         }
+//         head->next = removeElements(head->next, val);
+//         return head;
+//     }
+// };
